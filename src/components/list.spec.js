aa5da301727ebf0e62d18ad1855d2d976178d822
Antonio Morga <anotniomc9719@gmail.com>
2022-06-01 13:39:22 +0200

    Added test for list component

---
+++ src/components/list.spec.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { List } from "./list";
+
+describe("Given the component List", () => {
+  const mockTasks = [
+    {
+      id: 1,
+      title: "test 1",
+      responsible: "resp 1",
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: "test 2",
+      responsible: "resp 2",
+      isComplete: false,
+    },
+  ];
+  describe("When passing an array of task objects", () => {
+    test("Then a number of tasks should be in the document", () => {
+      render(<List tasks={mockTasks} />);
+
+      expect(screen.getAllByRole("listitem")).toHaveLength(mockTasks.length);
+
+      mockTasks.forEach((task) => {
+        expect(screen.getByText(task.title)).toBeInTheDocument();
+      });
+    });
+  });
+});
