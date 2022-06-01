9866afd131bdf9e410db768b364656f6d949eab5
Antonio Morga <anotniomc9719@gmail.com>
2022-06-01 13:48:50 +0200

    Added test for task component

---
+++ src/components/task.spec.js
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Task } from "./task";
+
+describe("Given the component Task", () => {
+  const mockTask = {
+    id: 1,
+    title: "test 1",
+    responsible: "resp 1",
+    isComplete: false,
+  };
+
+  const mockDeleteFunction = jest.fn();
+
+  describe("When calling it with a task", () => {
+    test("Then it should be rendered", () => {
+      // Arrange
+      render(<Task task={mockTask} />);
+
+      // Assert
+      expect(screen.getByText(mockTask.title)).toBeInTheDocument();
+    });
+  });
+
+  describe("When calling it with a task and then pressing delete", () => {
+    test("Then the passed function deleteTask should be called", () => {
+      // Arrange
+      render(<Task task={mockTask} deleteTask={mockDeleteFunction} />);
+
+      // Act
+      const deleteButton = screen.getByRole("button");
+      userEvent.click(deleteButton);
+
+      // Assert
+      expect(mockDeleteFunction).toHaveBeenCalledWith(mockTask.id);
+    });
+  });
+});
